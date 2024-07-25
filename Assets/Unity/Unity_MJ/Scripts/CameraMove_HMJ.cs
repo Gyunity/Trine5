e0811990d80a5eea3dff2ac6847e9a08caaadbeb
Hjj0731 <84975902+HongMyengJin@users.noreply.github.com>
2024-07-25 21:01:51 +0900

    카메라 러그 구현
    카메라가 조금 지연되게 플레이어를 추적하는 것을 구현

---
+++ Assets/Unity/Unity_MJ/Scripts/CameraMove_HMJ.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform target;
+    // 카메라가 따라갈 대상
+
+    public float smoothSpeed = 0.125f;
+    // 카메라의 따라가는 속도
+
+    public Vector3 offset;
+    // 대상과 카메라 사이의 거리
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        // 카메라의 목표 위치 계산
+        Vector3 desiredPosition = target.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+    }
+}
