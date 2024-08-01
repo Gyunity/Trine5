ac1f9aca82864d2ab023a159f55ac8e103f8e665
donkiote <74314213+donkiote@users.noreply.github.com>
2024-08-01 14:27:32 +0900

    BossSkillColl

--- Assets/Unity/Unity_BM/Scripts/BoxMove.cs
+++ Assets/Unity/Unity_BM/Scripts/BoxMove.cs
@@ -17,4 +17,16 @@ public class BoxMove : MonoBehaviour
     {
         transform.position += Vector3.down * speed * Time.deltaTime;
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        //PlayerWeapon에도
+        if (other.gameObject.tag == "Player" || other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            //공격
+            Debug.Log("박스 공격");
+            //GameObject explosion = Instantiate(explosionFactory);
+            //explosion.transform.position = transform.position;
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Unity/Unity_BM/Scripts/FireBallMove.cs
+++ Assets/Unity/Unity_BM/Scripts/FireBallMove.cs
@@ -7,13 +7,14 @@ public class FireBallMove : MonoBehaviour
 {
     //Player
     GameObject target;
+    public string targetName = "Player_Dummy";
     Vector3 dir;
     public float speed = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.Find("Player_Dummy");
+        target = GameObject.Find(targetName);
         dir =target.transform.position - transform.position;
         dir.Normalize();
         
@@ -23,5 +24,16 @@ public class FireBallMove : MonoBehaviour
     void Update()
     {
         transform.position += dir * speed * Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //PlayerWepon에도
+        if (other.gameObject.tag == "Player" || other.gameObject.layer == LayerMask.NameToLayer("Wall") || other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Debug.Log("불꽃 공격");
+            //GameObject explosion = Instantiate(explosionFactory);
+            //explosion.transform.position = transform.position;
+            Destroy(gameObject);
+        }
     }
 }
