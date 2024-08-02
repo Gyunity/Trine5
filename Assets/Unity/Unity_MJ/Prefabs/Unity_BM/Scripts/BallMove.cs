3048d21ed268d9b6d46eb73ce23b57de1bf52bc0
MetaAJ2023 <metaverse002023@gmail.com>
2024-08-02 09:47:00 +0900

    사진 바꿈

---
+++ Assets/Unity/Unity_MJ/Prefabs/Unity_BM/Scripts/BallMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    Rigidbody rd;
+    public int movespeed = 10;
+
+    //이펙트
+    public GameObject explosionFactory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rd.velocity += Vector3.right* movespeed * Time.deltaTime;
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        
+        //만약에 부딪힌 오브젝트가 Player, Player weapon, wall 이면 
+        if (other.gameObject.tag == "Player" || other.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            Debug.Log("볼 공격");
+            //GameObject explosion = Instantiate(explosionFactory);
+            //explosion.transform.position = transform.position;
+            Destroy(gameObject);
+        }
+        //삭제해라
+        
+
+        //그리고
+    }
+
+}
---
+++ Assets/Unity/Unity_MJ/Prefabs/Unity_BM/Scripts/BoxFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxFactory : MonoBehaviour
+{
+    public GameObject boxPrefab;
+    public float delay = 1f;
+    public int boxCount = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BoxGo());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    IEnumerator BoxGo()
+    {
+        
+        //박스를 랜덤한 위치에 위치한다.
+        for (int i = 0; i < boxCount; i++)
+        {
+            int rd = Random.Range(1, 10);
+            Instantiate(boxPrefab, new Vector3(rd, this.transform.position.y, 0), Quaternion.identity);
+            yield return new WaitForSeconds(delay);
+            rd = 0;
+        }
+    }
+}
