8583169363f7dcc7b141a32777e789b1633cb6f5
Hjj0731 <84975902+HongMyengJin@users.noreply.github.com>
2024-08-12 01:12:35 +0900

    화살 타입 추가
    파이어, 아이스 타입 추가
    공격력 설정 가능

--- Assets/Unity/Unity_MJ/Scripts/PlatformDrop_HMJ.cs
+++ Assets/Unity/Unity_MJ/Scripts/PlatformDrop_HMJ.cs
@@ -19,47 +19,47 @@ public class PlatformDrop_HMJ : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        // 충돌한 콜라이더 체크
-        foreach (ContactPoint contact in collision.contacts)
-        {
-            Collider collider = contact.thisCollider;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    // 충돌한 콜라이더 체크
+    //    foreach (ContactPoint contact in other.contacts)
+    //    {
+    //        Collider collider = contact.thisCollider;
 
-            if (colliders[0] == collider) // 0번 - 왼쪽 충돌
-            {
-                int a = 0;
-                a++;
-                // 충돌한 콜라이더 실행
-            }
-            else if (colliders[1] == collider) // 1번
-            {
-                int b = 0;
-                b++;
-                // 충돌한 콜라이더 실행
-            }
-        }
-    }
+    //        if (colliders[0] == collider) // 0번 - 왼쪽 충돌
+    //        {
+    //            int a = 0;
+    //            a++;
+    //            // 충돌한 콜라이더 실행
+    //        }
+    //        else if (colliders[1] == collider) // 1번
+    //        {
+    //            int b = 0;
+    //            b++;
+    //            // 충돌한 콜라이더 실행
+    //        }
+    //    }
+    //}
 
-    private void OnCollisionExit(Collision collision)
-    {
-        // 충돌한 콜라이더 체크
-        foreach (ContactPoint contact in collision.contacts)
-        {
-            Collider collider = contact.thisCollider;
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    // 충돌한 콜라이더 체크
+    //    foreach (ContactPoint contact in collision.contacts)
+    //    {
+    //        Collider collider = contact.thisCollider;
 
-            if (colliders[0] == collider) // 0번
-            {
-                int a = 0;
-                a++;
-                // 충돌한 콜라이더 실행
-            }
-            else if (colliders[1] == collider) // 1번
-            {
-                int b = 0;
-                b++;
-                // 충돌한 콜라이더 실행
-            }
-        }
-    }
+    //        if (colliders[0] == collider) // 0번
+    //        {
+    //            int a = 0;
+    //            a++;
+    //            // 충돌한 콜라이더 실행
+    //        }
+    //        else if (colliders[1] == collider) // 1번
+    //        {
+    //            int b = 0;
+    //            b++;
+    //            // 충돌한 콜라이더 실행
+    //        }
+    //    }
+    //}
 }
