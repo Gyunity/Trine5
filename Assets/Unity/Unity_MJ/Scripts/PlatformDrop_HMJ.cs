1b520aac4aa03e7b2a0ae09f8ffecb03d7b1849a
Hjj0731 <84975902+HongMyengJin@users.noreply.github.com>
2024-08-12 08:08:28 +0900

    통나무 왼쪽 오른쪽에 따라 회전 기능 추가 - 플레이어와 트리거시(왼쪽 - 오른쪽 따로 작동)
    충돌체를 3개를 사용해서 플레이어가 탈 수 있는 충돌체(콜리전), 왼쪽 충돌체(트리거), 오른쪽 충돌체(트리거) 플레이어가 탑승했을 경우 z축으로 내려가도록 구현

--- Assets/Unity/Unity_MJ/Scripts/PlatformDrop_HMJ.cs
+++ Assets/Unity/Unity_MJ/Scripts/PlatformDrop_HMJ.cs
@@ -1,65 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlatformDrop_HMJ : MonoBehaviour
 {
-    List<Collider> colliders;
+    public float rotationSpeed = 1.0f;  // 회전 속도
+    private Quaternion targetRotation;  // 목표 회전
+    private bool shouldRotate = false;  // 회전을 할지 여부
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        colliders = new List<Collider>(GetComponentsInChildren<Collider>()); // 모든 콜라이더 가져오기
-
+        // 초기 목표 회전은 현재 회전
+        targetRotation = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shouldRotate)
+        {
+            // 현재 회전에서 목표 회전으로 부드럽게 회전
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            // 목표 회전과 매우 가까워지면 회전 중지
+            if (Quaternion.Angle(transform.rotation, targetRotation) < 0.1f)
+            {
+                transform.rotation = targetRotation;  // 목표 회전에 도달
+                shouldRotate = false;  // 회전 완료
+            }
+        }
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    // 충돌한 콜라이더 체크
-    //    foreach (ContactPoint contact in other.contacts)
-    //    {
-    //        Collider collider = contact.thisCollider;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Contains("Player"))
+        {
 
-    //        if (colliders[0] == collider) // 0번 - 왼쪽 충돌
-    //        {
-    //            int a = 0;
-    //            a++;
-    //            // 충돌한 콜라이더 실행
-    //        }
-    //        else if (colliders[1] == collider) // 1번
-    //        {
-    //            int b = 0;
-    //            b++;
-    //            // 충돌한 콜라이더 실행
-    //        }
-    //    }
-    //}
+            // 충돌한 위치가 나무통의 왼쪽인지 오른쪽인지 확인
+            Vector3 contactPoint = other.ClosestPoint(transform.position);
+            Vector3 direction = contactPoint - transform.position;
 
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    // 충돌한 콜라이더 체크
-    //    foreach (ContactPoint contact in collision.contacts)
-    //    {
-    //        Collider collider = contact.thisCollider;
+            if (direction.x < 0)  // 나무통의 왼쪽에서 충돌
+            {
+                // 왼쪽 충돌 시 시계방향으로 45도 회전
 
-    //        if (colliders[0] == collider) // 0번
-    //        {
-    //            int a = 0;
-    //            a++;
-    //            // 충돌한 콜라이더 실행
-    //        }
-    //        else if (colliders[1] == collider) // 1번
-    //        {
-    //            int b = 0;
-    //            b++;
-    //            // 충돌한 콜라이더 실행
-    //        }
-    //    }
-    //}
+                float targetZRotation = Mathf.Clamp(transform.eulerAngles.z + 45, -60.0f, 110.0f);
+                targetRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, targetZRotation);
+                shouldRotate = true;
+            }
+            else
+            {
+                float targetZRotation = Mathf.Clamp(transform.eulerAngles.z - 45, 70.0f, 110.0f);
+                targetRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, targetZRotation);
+                shouldRotate = true;
+            }
+        }
+    }
+
 }
+                                            
